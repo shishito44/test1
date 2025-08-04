21fa0a1ef28fa7dd771be8dba4f0d2dea84bdc1f
DESKTOP-NKURHCG\user <shunshun6640@outlook.jp>
2025-08-04 14:12:12 +0900

    オウム返しをするプログラム

--- practice/ConsoleApp1/Program.cs
+++ practice/ConsoleApp1/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("オウム返しをするプログラム（finで終了）");
 while (true)
 {
-    Console.WriteLine("Hello, World!");
-    int a = 42;
-    int b = 119;
-    int c = a + b;
-    Console.WriteLine(c);
-    Console.ReadKey();
+    string str;
+    str = Console.ReadLine();
+    if (str == string.Empty)
+    {
+        Console.WriteLine("文字を入力してください");
+    }
+    else if (str == "fin")
+    {
+        Console.WriteLine("終了");
+        break;
+    }
+    Console.WriteLine(str);
 }
