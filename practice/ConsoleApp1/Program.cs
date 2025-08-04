2c26df4e48659ce5704c2effe4952e42d8ad940f
DESKTOP-NKURHCG\user <shunshun6640@outlook.jp>
2025-08-04 13:36:30 +0900

    メインループ処理

---
+++ practice/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Hello, World!");
+    int a = 42;
+    int b = 119;
+    int c = a + b;
+    Console.WriteLine(c);
+    Console.ReadKey();
+}
